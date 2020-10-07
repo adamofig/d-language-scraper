63026270a2824c347c2a53997fcced641338be53
Adamo Figueroa <jordan.figu@gmail.com>
2020-10-07 14:22:57 -0500

    Agrengando instrucciones

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+import dryscrape
+from bs4 import BeautifulSoup
+
+dryscrape.start_xvfb()
+
+sess = dryscrape.Session()
+url = "https://translate.google.com.mx/#view=home&op=translate&sl=en&tl=es&text=raise"
+sess.visit(url)
+source = sess.body()
+
+soup = BeautifulSoup(source,'lxml')
+
+print(soup)
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World2"}
+    return {"Hello": "World22"}
+
 
--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 ### Construir docker 
 
-docker build -t d-scrapper .
+
+    docker build -t d-scraper .
+
+Iniciando el contenedor
+
+    docker run -it -p 8000:8080 -v %cd%:/usr/src/app --name scrap d-scraper
+
+Prendiendo y apagando ambiente 
+
+    docker start -a scrap
