5cacdd18f7814a6d9e3411ede52f423cfd69168f
Adamo Figueroa <jordan.figu@gmail.com>
2020-11-02 17:43:32 -0600

    Agregando drive de google

--- main.py
+++ main.py
@@ -9,27 +9,30 @@ app.add_middleware( CORSMiddleware, allow_origins=["*"],
     allow_credentials=True,allow_methods=["*"], allow_headers=["*"]
 )
 
-# import dryscrape
-#from bs4 import BeautifulSoup
-
-# dryscrape.start_xvfb()
-"""
-sess = dryscrape.Session()
-url = "https://translate.google.com.mx/#view=home&op=translate&sl=en&tl=es&text=raise"
-sess.visit(url)
-source = sess.body()
-
-soup = BeautifulSoup(source,'lxml')
-
-print(soup)"""
 
 @app.get("/")
 def read_root():
     return {"Scrapper": "Bievenido al scraper, no hay documentación"}
 
-# @app.get("/translate/{palabra}")
-# def translate(palabra):
-#  return functions.buscar(palabra)
+
+
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+# options.add_argument('window-size=1200x600')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+browser = webdriver.Chrome(chrome_options=options)
+#If the chromedriver is not set in the PATH environment variable, specify the chromedriver location with the executable_path option.
+#browser = webdriver.Chrome(chrome_options=options, executable_path="/usr/local/bin/chromedriver")
+
+url = "http://google.com"
+
+browser.get(url)
+browser.save_screenshot("Website.png")
+browser.quit()
 
 
 
