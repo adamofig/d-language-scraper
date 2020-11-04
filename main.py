298c615b10e54226e4e8a7f4040cdbbd77855fdb
Adamo Figueroa <jordan.figu@gmail.com>
2020-11-04 14:39:57 -0600

    Regresando a versíon con scrapping

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from selenium import webdriver
-
-import browser
 import functions
 
 app = FastAPI()
-app.add_middleware( CORSMiddleware, allow_origins=["*"], allow_credentials=True,allow_methods=["*"], allow_headers=["*"])
+
+app.add_middleware( CORSMiddleware, allow_origins=["*"],
+    allow_credentials=True,allow_methods=["*"], allow_headers=["*"],
+)
+
+import dryscrape
+#from bs4 import BeautifulSoup
+
+dryscrape.start_xvfb()
+"""
+sess = dryscrape.Session()
+url = "https://translate.google.com.mx/#view=home&op=translate&sl=en&tl=es&text=raise"
+sess.visit(url)
+source = sess.body()
+
+soup = BeautifulSoup(source,'lxml')
+
+print(soup)"""
 
 @app.get("/")
 def read_root():
-    return {"Scrapper": "Bienvenido al scraper, no hay documentación"}
-
-@app.get("/word/{word}")
-def get_word(word):
-    print("Buscando la palabra", word)
-    word_api = browser.get_word_from_api(word)
-    word_dict = browser.extract_data_from_word(word_api)
-    print(word_dict)
-    translations = browser.scrap_transalation(word)
-    diccionario = {**word_dict, **translations}
-    return diccionario
-
+    return {"Scrapper": "Bievenido al scraper, no hay documentación"}
 @app.get("/translate/{palabra}")
 def translate(palabra):
  return functions.buscar(palabra)
-
 
 
 
